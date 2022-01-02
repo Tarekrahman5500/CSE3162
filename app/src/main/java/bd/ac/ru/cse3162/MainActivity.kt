0875b78eccd04a837c5f757318d606724409673c
Tarek Rahman <tr617739@gmail.com>
2022-01-02 10:36:23 +0600

    Update UI

--- app/src/main/java/bd/ac/ru/cse3162/MainActivity.kt
+++ app/src/main/java/bd/ac/ru/cse3162/MainActivity.kt
@@ -2,11 +2,13 @@ package bd.ac.ru.cse3162
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import bd.ac.ru.cse3162.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+  lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
